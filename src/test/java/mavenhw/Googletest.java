03ac0c811a9dca84c54ccde49537e5c295d8c097
GigiMann27 <gmann0827@gmail.com>
2018-12-08 15:01:26 -0500

    Added title test

---
+++ src/test/java/mavenhw/Googletest.java
@@ -0,0 +1,36 @@
+package mavenhw;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Googletest {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "/Users/Gurpreet/eclipse-workspace/mavenhw/Drivers/chromedriver");
+
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void findTitle() {
+		String title=driver.getTitle();
+		System.out.println(title);
+	}
+
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+}
